9b0f47c37133a60342279c56d8d1fed110c7667a
yjiang <907292671@qq.com>
2020-06-04 13:49:19 +0800

    close nutrition questionnaire

--- src/main/java/com/yjiang/base/init/StartPingService.java
+++ src/main/java/com/yjiang/base/init/StartPingService.java
@@ -2,7 +2,6 @@ package com.yjiang.base.init;
 
 import com.yjiang.base.core.util.MailUtils;
 import com.yjiang.base.modular.health.service.HealthCareService;
-import com.yjiang.base.modular.system.model.HealthUsers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -21,38 +20,22 @@ public class StartPingService implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        for (int i = 0; i < 5; i++) {
-            try {
-//                nutritionService.singlePersonProcess(new HealthUsers(0, "王冬梅", "25～30岁以下", "女", "大专/本科","医务人员", "", 0, 0), false, false);
-//                nutritionService.singlePersonProcess(new HealthUsers(0, "王加徐", "55～60岁以下", "男", "高中/职高/中专","医务人员", "", 0, 0), false, false);
-//                nutritionService.singlePersonProcess(new HealthUsers(0, "王树喜", "55～60岁以下", "男", "高中/职高/中专","医务人员", "", 0, 0), false, false);
-//                nutritionService.singlePersonProcess(new HealthUsers(0, "曹兴亚", "55～60岁以下", "男", "高中/职高/中专","医务人员", "", 0, 0), false, false);
-//                nutritionService.singlePersonProcess(new HealthUsers(0, "王震", "45～50岁以下", "男", "高中/职高/中专","医务人员", "", 0, 0), false, false);
-                new Thread(() -> {
-                    try {
-                        nutritionService.process(1, true);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-                new Thread(() -> {
-                    try {
-                        healthCareService.process(1, true);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }).start();
-                break;
-            } catch (Exception e) {
-                e.printStackTrace();
-                MailUtils.sendSimpleMail("907292671@qq.com", "healthCare " + e.getMessage(), healthCareService.getUrl());
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
                 try {
-                    Thread.sleep(600 * 1000);
-                } catch (InterruptedException x) {
-                    x.printStackTrace();
+                    healthCareService.process(1, true);
+                    break;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    MailUtils.sendSimpleMail("907292671@qq.com", "healthCare " + e.getMessage(), healthCareService.getUrl());
+                    try {
+                        Thread.sleep(600 * 1000);
+                    } catch (InterruptedException x) {
+                        x.printStackTrace();
+                    }
                 }
             }
-        }
+        }).start();
     }
 
 }
