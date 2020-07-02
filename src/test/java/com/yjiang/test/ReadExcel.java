0c3d8450e296b9af175c8c5ab611d328bbb86333
yjiang <907292671@qq.com>
2020-07-02 13:50:23 +0800

    excel process

--- src/test/java/com/yjiang/test/ReadExcel.java
+++ src/test/java/com/yjiang/test/ReadExcel.java
@@ -12,14 +12,15 @@ import java.util.Map;
 
 public class ReadExcel {
     public static void main(String[] args) throws Exception {
-        List<Map<String,String>> list1 = readFile("D:/2019东小店.xlsx", 0);
-        List<Map<String, String>> list2 = readFile("D:/2020 4-4上报新东小店乡低收入户签约 .xlsx", 0);
+        List<Map<String,String>> list1 = readFile("D:/系统导出.xlsx", 0);
+        List<Map<String, String>> list2 = readFile("D:/建档立卡最新.xlsx", 0);
         List<Map<String, String>> l2 = new ArrayList<>();
         List<Map<String, String>> l1 = new ArrayList<>();
         for (Map<String, String> map2 : list2) {
             boolean flag = false;
             for (Map<String, String> map1 : list1) {
-                if((map1.get("姓名").equals(map2.get("姓名")) && map1.get("身份证号").contains(map2.get("身份证号")))){
+                String  sfzh = map1.get("身份证号码").startsWith("'")? map1.get("身份证号码").substring(1) : map1.get("身份证号码");
+                if(sfzh.equals(map2.get("身份证号"))){
                     flag = true;
                     break;
                 }
@@ -31,7 +32,8 @@ public class ReadExcel {
         for (Map<String, String> map1 : list1) {
             boolean flag = false;
             for (Map<String, String> map2 : list2) {
-                if((map1.get("姓名").equals(map2.get("姓名")) && map1.get("身份证号").contains(map2.get("身份证号")))){
+                String  sfzh = map1.get("身份证号码").startsWith("'")? map1.get("身份证号码").substring(1) : map1.get("身份证号码");
+                if(sfzh.equals(map2.get("身份证号"))){
                     flag = true;
                     break;
                 }
@@ -40,9 +42,9 @@ public class ReadExcel {
                 l1.add(map1);
             }
         }
-        printList(l1, null,"2019不在2020");
-        printList(l2, null,"2020不在2019");
-
+        printList(l1, "D:/建档导出比较.xlsx","导出不在建档",  "户主姓名", "成员姓名", "身份证号码");
+        printList(l2, "D:/建档导出比较.xlsx","建档不在导出", "姓名", "身份证号");
+/*
         List<Map<String, String>> l3 = new ArrayList<>();
         List<Map<String, String>> l4 = new ArrayList<>();
 
@@ -75,7 +77,7 @@ public class ReadExcel {
             }
         }
 
-        printList(list2, null, "贫困户属性", "行政村", "贫困户属性");
+        printList(list2, null, "贫困户属性", "行政村", "贫困户属性");*/
     }
 
     public static List<Map<String,String>> readFile(String fileName, int sheetNum) {
@@ -93,12 +95,12 @@ public class ReadExcel {
             //获取最大行数
             int rownum = sheet.getPhysicalNumberOfRows();
             //获取第一行
-            row = sheet.getRow(2);
+            row = sheet.getRow(0);
             //获取最大列数
             int colnum = row.getPhysicalNumberOfCells();
             List<String> columns = new ArrayList<>();
-            for (int i = 2; i<=rownum; i++) {
-                if(i==2){
+            for (int i = 0; i<=rownum; i++) {
+                if(i==0){
                     for (int j=0;j<colnum;j++){
                         columns.add((String) getCellFormatValue(row.getCell(j)));
                     }
@@ -124,7 +126,7 @@ public class ReadExcel {
 
     public static void printList(List<Map<String,String>> list, List<Map<String,String>> list2, String name, String... s) throws Exception {
         //遍历解析出来的list
-        String filePath = "D:/2019-2020东小店数据.xlsx";
+        String filePath = "D:/导出建档比较数据.xlsx";
         File file = new File(filePath);
         if(!file.exists()){
             createExcel(filePath);
@@ -160,6 +162,37 @@ public class ReadExcel {
                     for (int i1 = 1; i1 <= s.length; i1++) {
                         row.createCell(1+i1).setCellValue(map.get(s[i1-1]));
                     }
+                }
+            }
+        }
+        wb.write(fileOutputStream);
+    }
+
+    public static void printList(List<Map<String,String>> list,String filePath, String sheetName, String... s) throws Exception {
+        //遍历解析出来的list
+        File file = new File(filePath);
+        if(!file.exists()){
+            createExcel(filePath);
+        }else{
+            file.delete();
+            createExcel(filePath);
+        }
+        Workbook wb = getWorkbook(filePath);
+        FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+        Sheet sheet = wb.getSheet("Sheet1");
+        if(sheet != null){
+            wb.setSheetName(0, sheetName);
+        }else{
+            sheet = wb.createSheet(sheetName);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            Map<String, String> map = list.get(i);
+            Row row = sheet.createRow(i);
+
+            if(s != null){
+                for (int i1 = 0; i1 < s.length; i1++) {
+                    row.createCell(i1).setCellValue(map.get(s[i1]));
                 }
             }
         }
